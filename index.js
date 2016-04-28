590bb9957a1624fa3a91d3d74113fa00d8003e3e
Hsiaoyi Hsu <xreamxu@gmail.com>
2016-04-29 03:10:44 +0800

    [+] 改为 DisplayName

--- index.js
+++ index.js
@@ -2,26 +2,45 @@ require('shelljs/global')
 const path = require('path')
 const pinyin = require('pinyin')
 
+const combinations = (array) => {
+  function combine(list) {
+    let prefixes, combinations
+    if(list.length === 1) return list[0]
+    prefixes = list[0]
+    combinations = combine(list.slice(1))
+    return prefixes.reduce((memo, prefix) => memo.concat(combinations.map(combination => [prefix].concat(combination))), [])
+  }
+  return combine(array)
+}
+const pinyinArrayToStr = (input) => {
+  return combinations(input).map(item => {
+    return item.join('')
+  }).join(' ')
+}
+
 exec('mdfind kind:app', (code, stdout, stderr) => {
   if (code !== 0) return process.exit(1)
-  const appPaths = stdout.split('\n').map(appPath => {
-    if (!appPath.match(/[\u4E00-\u9FA5\uF900-\uFA2D]/)) return
-    const name = path.basename(appPath, '.app')
-    const pinyinNormal = pinyin(name, {
-      heteronym: true,
-      segment: true,
-      style: pinyin.STYLE_NORMAL
-    }).join('')
-    const pinyinFirstLetter = pinyin(name, {
-      heteronym: true,
-      segment: true,
-      style: pinyin.STYLE_FIRST_LETTER
-    }).join('')
-    appPath = appPath.replace(/ /g, '\\ ')
-
-    exec(`osascript -e 'tell application "Finder" to set comment of (POSIX file "${appPath}" as alias) to "${pinyinNormal} ${pinyinFirstLetter}"'`, (code, stdout, stderr) => {
-      if (code !== 0) return process.exit(2)
-      console.log(`🚀 [${name}]: ${pinyinNormal} ${pinyinFirstLetter}`)
+  stdout.split('\n').forEach(appPath => {
+    if (!appPath) return
+    exec(`mdls -name kMDItemDisplayName '${appPath}'`, (code, stdout, stderr) => {
+      if (code !== 0) return
+      const match = stdout.match(/.*?"(.*?[\u4E00-\u9FA5\uF900-\uFA2D]+?.*?)"/)
+      if (!match) return
+      const name = match[1]
+      const pinyinNormal = pinyinArrayToStr(pinyin(name, {
+        heteronym: true,
+        segment: true,
+        style: pinyin.STYLE_NORMAL
+      }))
+      const pinyinFirstLetter = pinyinArrayToStr(pinyin(name, {
+        heteronym: true,
+        segment: true,
+        style: pinyin.STYLE_FIRST_LETTER
+      }))
+      exec(`osascript -e 'tell application "Finder" to set comment of (POSIX file "${appPath}" as alias) to "${pinyinNormal} ${pinyinFirstLetter}"'`, (code, stdout, stderr) => {
+        if (code !== 0) return process.exit(3)
+        console.log(`🚀 [${name}]: ${pinyinNormal} ${pinyinFirstLetter}`)
+      })
     })
   })
 })
